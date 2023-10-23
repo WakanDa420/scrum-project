f5deb1ef7d910eae82f2690e7c2cd3a04c29db3e
WakanDa420 <129581791+WakanDa420@users.noreply.github.com>
2023-10-22 22:27:50 -0500

    Initial commit

---
+++ app/Http/Controllers/SprintController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\sprint;
+use App\Http\Requests\StoresprintRequest;
+use App\Http\Requests\UpdatesprintRequest;
+
+class SprintController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoresprintRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(sprint $sprint)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(sprint $sprint)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatesprintRequest $request, sprint $sprint)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(sprint $sprint)
+    {
+        //
+    }
+}
